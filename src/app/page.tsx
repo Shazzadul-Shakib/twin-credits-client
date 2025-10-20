9c421efd2640190975c5e3bc06457a0d7c571d54
Shazzadul-Shakib <shakib1186@gmail.com>
2025-10-20 13:40:18 +0600

    init: project initialized with folder structure

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-primary underline">Hello, world!</h1>
+    </div>
+  );
+}
